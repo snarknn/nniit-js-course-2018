f39899388fd4cb882f10288e9b92d01080cf5448
Mikhail Shashanov <mikhail.shashanov@radiogigabit.com>
2018-06-16 18:52:49 +0300

    JS home work #1

--- todoList/todo.js
+++ todoList/todo.js
@@ -36,23 +36,72 @@ window.onload = function(){
         + t.getSeconds();
     }
 
+    // Task #1 (Reduce)
+    console.log('Task 1');
+    var matrix = [
+        [1, 2, 3, 12],
+        [4, 5, 6, -5],
+        [7, 8, 9, 0]
+      ];
+    
+    var result = matrix.reduce(function(prev, curr) 
+        {return curr.map(function(val, idx2){return val + prev[idx2]})}, [0, 0, 0, 0]);
+    result.forEach(function(item) {console.log(item)});
 
+    // Task #2 (Fibonacci)
+    console.log('Task 2');
+    var num1 = 78;
+    function fib1(n) {
+        if (n == 0) {
+            return 0;
+        }
+        else if (n == 1) {
+            return 1;
+        }
+
+        var arr = [0, 1];
+        while (arr.length < n+1) {
+            arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+        }
+        //console.log(arr);
+        return arr[arr.length - 1];
+    }
+    var fibRes1 = fib1(num1);
+    console.log(fibRes1);
+
+    function fib2(n) { 
+        if (n == 0) {
+            return 0;
+        }
+        else if (n == 1) {
+            return 1;
+        }
+
+        var a = 0, b = 1, c = a + b;
+        var count = 2;
+        while (count < n) {
+            a = b;
+            b = c;
+            c = a + b;
+            count++;
+        }
+        return c;
+    }
+    var fibRes2 = fib2(num1);
+    console.log(fibRes2);
+
+    // Task #3
+    console.log('Task 3');
     function getTimer(){
         var i = 0;
-
         return function(maxI){
-            if (i < maxI) {
-                setInterval(function(){
+            setInterval(function(){
+                if (i < maxI) {
                     console.log(maxI + '-' + i++);
-                }, 1000)
-            }
+                }
+            }, 1000)
         }
     }
-
     var t = getTimer();
-    t(10);
-
-
- 
-
+    t(12);
 }
